ceb3815ecb09d415a0ac287bab254f4c7129c2ce
Mikhael <midebar@gmail.com>
2024-04-17 22:30:21 +0700

    Finished

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# tugas_tracker
+## tugas-tracker
 
-A new Flutter project.
+### Tugas 7
+#### Apa perbedaan utama antara stateless dan stateful widget dalam konteks pengembangan aplikasi Flutter?
 
-## Getting Started
+#### Sebutkan seluruh widget yang kamu gunakan untuk menyelesaikan tugas ini dan jelaskan fungsinya masing-masing.
 
-This project is a starting point for a Flutter application.
+#### Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas secara step-by-step (bukan hanya sekadar mengikuti tutorial)
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tugas_tracker/main.dart';
+import 'package:tugas_tracker/menu.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('SnackBar control test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    const String helloSnackBar = 'Kamu telah menekan tombol Tambah Item';
+    const Key tapTarget = Key('Lihat Item');
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Builder(builder: (BuildContext context) {
+          return GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text(helloSnackBar),
+                duration: Duration(seconds: 2),
+              ));
+            },
+            behavior: HitTestBehavior.opaque,
+            child: Container(
+              padding: const EdgeInsets.all(
+                  16.0), // Set the padding to add whitespace around the Container
+              height: 100.0,
+              width: 100.0,
+              key: tapTarget,
+            ),
+          );
+        }),
+      ),
+    ));
+    expect(find.text(helloSnackBar), findsNothing);
+    await tester.tap(find.byKey(tapTarget));
+    expect(find.text(helloSnackBar), findsNothing);
+    await tester.pump(); // schedule animation
+    expect(find.text(helloSnackBar), findsOneWidget);
   });
 }
