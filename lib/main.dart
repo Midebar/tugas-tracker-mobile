90d40ad8dd1ef1c1f6dcd4ab00febcb3fe3835cb
Midebar <midebar@gmail.com>
2024-04-29 15:17:24 +0700

    Fix bug deadline add tugas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 import 'package:tugas_tracker/screens/main_menu.dart';
 
 void main() {
+  tz.initializeTimeZones();
   runApp(const MyApp());
 }
--- lib/screens/tracker_form.dart
+++ lib/screens/tracker_form.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 import 'package:tugas_tracker/screens/main_menu.dart';
 import 'package:tugas_tracker/widgets/left_drawer.dart';
@@ -19,6 +20,7 @@ class _TrackerFormPageState extends State<TrackerFormPage> {
   String _name = "";
   int _amount = 0;
   String _description = "";
+  DateTime? _deadline;
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
@@ -110,6 +112,31 @@ class _TrackerFormPageState extends State<TrackerFormPage> {
                   },
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  readOnly: true,
+                  onTap: () => _selectDate(context),
+                  decoration: InputDecoration(
+                    hintText: "Deadline",
+                    labelText: "Deadline",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  controller: _deadline == null
+                      ? null
+                      : TextEditingController(
+                          text:
+                              "${_deadline!.day}/${_deadline!.month}/${_deadline!.year}"),
+                  validator: (String? value) {
+                    if (_deadline == null) {
+                      return "Deadline tidak boleh kosong!";
+                    }
+                    return null;
+                  },
+                ),
+              ),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: Padding(
@@ -130,8 +157,9 @@ class _TrackerFormPageState extends State<TrackerFormPage> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text('Judul: $_name'),
-                                    Text('Halaman: $_amount'),
+                                    Text('Jumlah: $_amount'),
                                     Text('Deskripsi: $_description'),
+                                    Text('Deadline: $_deadline'),
                                   ],
                                 ),
                               ),
@@ -147,19 +175,23 @@ class _TrackerFormPageState extends State<TrackerFormPage> {
                             );
                           },
                         );
+                        final utcDeadline = _deadline != null
+                            ? _convertToUtc(_deadline!)
+                            : null;
                         final response = await request.postJson(
                           "http://127.0.0.1:8000/create-flutter/",
-                          jsonEncode(<String, String>{
+                          jsonEncode(<String, dynamic>{
                             'name': _name,
-                            'page': _amount.toString(),
+                            'amount': _amount.toString(),
                             'description': _description,
+                            'deadline': utcDeadline?.toIso8601String(),
                           }),
                         );
                         if (context.mounted) {
                           if (response['status'] == 'success') {
                             ScaffoldMessenger.of(context)
                                 .showSnackBar(const SnackBar(
-                              content: Text("Buku baru berhasil disimpan!"),
+                              content: Text("Tugas baru berhasil disimpan!"),
                             ));
                             Navigator.pushReplacement(
                               context,
@@ -186,5 +218,26 @@ class _TrackerFormPageState extends State<TrackerFormPage> {
             ]),
           ),
         ));
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _deadline ?? DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _deadline) {
+      setState(() {
+        _deadline = picked;
+      });
+    }
+  }
+
+  tz.TZDateTime _convertToUtc(DateTime dateTime) {
+    // Get the current timezone
+    final location = tz.getLocation('UTC');
+    // Create a TZDateTime object with UTC timezone
+    return tz.TZDateTime.from(dateTime, location);
   }
 }
